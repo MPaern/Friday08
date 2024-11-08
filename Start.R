305f5219f25d616b68f4a693bc1f5cf1422c0059
MaeP <maris.parn@gmail.com>
2024-11-08 11:05:24 +0100

    protect_main

--- Start.R
+++ Start.R
@@ -9,3 +9,13 @@ polygon(c(0, 0, 0.5, 0.5), c(0, 0.5, 0.5, 0))
 #just testing out
 plot.new()
 polygon(c(0, 0, 1, 0.5), c(0, 1, 1, 0))
+
+usethis::use_git_hook(
+  hook = "pre-commit",
+  script = '#!/bin/sh
+  branch="$(git rev-parse --abbrev-ref HEAD)"
+  if [ "$branch" = "main" ]; then
+  echo "You cannot commit directly to main branch"
+  exit 1
+  fi'
+)
